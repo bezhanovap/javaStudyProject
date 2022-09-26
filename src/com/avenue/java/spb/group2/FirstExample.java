116b1683684eaaf7456ad12de780f7334731ef83
Roum-s <roum-s@mail.ru>
2022-09-26 14:30:45 +0300

    First code java comment

--- src/com/avenue/java/spb/group2/FirstExample.java
+++ src/com/avenue/java/spb/group2/FirstExample.java
@@ -2,6 +2,8 @@ package com.avenue.java.spb.group2;
 
 public class FirstExample {
     public static void main(String[] args) {
+
+        //Text output
         System.out.println("Hello");
     }
 }
