40bd8ca0f6ca2f34165f21927d45a5d7a194b7a0
Roum-s <roum-s@mail.ru>
2022-11-18 23:07:38 +0300

    Lesson 17

--- src/com/avenue/java/spb/group2/lesson17/Application.java
+++ src/com/avenue/java/spb/group2/lesson17/Application.java
@@ -1,14 +1,27 @@
 package com.avenue.java.spb.group2.lesson17;
 
-import org.apache.log4j.Logger;
-
-import java.util.Arrays;
-
 public class Application {
-    public static final Logger LOGGER = Logger.getLogger(Application.class);
+    // public static final Logger LOGGER = Logger.getLogger(Application.class);
 
     public static void main(String[] args) {
-        LOGGER.debug("message");
-        System.out.println("logger has started");
+        // LOGGER.debug("message");
+        //System.out.println("logger has started");
+
+        MyFirstThread myFirstThread = new MyFirstThread();
+        myFirstThread.start();
+
+        Thread mySecondThread = new Thread(new MySecondThread());
+        mySecondThread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Поток main печатает " + i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
---
+++ src/com/avenue/java/spb/group2/lesson17/MySecondThread.java
@@ -0,0 +1,16 @@
+package com.avenue.java.spb.group2.lesson17;
+
+public class MySecondThread implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 1000; i < 1030; i++) {
+            System.out.println(Thread.currentThread().getName() +" "+ i);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
